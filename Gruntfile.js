7063341d24eee1e8b2fcb277664eddf7489082ee
Nikas Praninskas <nikaspran@gmail.com>
2016-11-25 15:16:18 +0200

    Fix build

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,14 +6,16 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jasmine: {
-      src: ['app/js/**/*.js', '!app/js/**/*.spec.js'],
-      options: {
-        vendor: [
-          'app/bower_components/angular/angular.js',
-          'app/bower_components/angular-ui-router/release/angular-ui-router.js',
-          'app/bower_components/angular-mocks/angular-mocks.js'
-        ],
-        specs: 'app/js/**/*.spec.js'
+      dev: {
+        src: ['app/js/**/*.js', '!app/js/**/*.spec.js'],
+        options: {
+          vendor: [
+            'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-ui-router/release/angular-ui-router.js',
+            'app/bower_components/angular-mocks/angular-mocks.js'
+          ],
+          specs: 'app/js/**/*.spec.js'
+        }
       }
     },
     connect: {
